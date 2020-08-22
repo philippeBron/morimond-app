37bbbbb1b4b2f1fcb80a84787a24895e7e0a84e1
Philippe Bron <philippe.bron@gmail.com>
2020-08-22 21:09:46 +0200

    💄 add data table and clean code

--- fouilles.js
+++ fouilles.js
@@ -120,8 +120,11 @@ function ExcelDateToJSDate(serial) {
     let categorie = null
     let stratUnit = null
 
-    // hide map because selection changed
+    // hide map when selection changed
     divMap.style.visibility = "hidden"
+
+    // delete data table when selection changed
+    document.getElementById('dataTable').innerHTML = ""
 
 
     // check selected fields
@@ -417,7 +420,7 @@ function showMap(scale) {
                 us: stratUnit
             }, (succ, data) => {
                 if(succ) {
-                    displayMap(data, 'multi', scale)
+                    displayMap(data, 'mono', scale)
                 } else {
                     console.log('An error has occured.')
                     console.log(`Message: ${data}`)
@@ -473,6 +476,7 @@ function displayMap(data, type, scale){
     const dlButton = document.getElementById('download')
     const width = canvas.width = (4218)
     const height = canvas.height = (5318)
+    const dataTable = document.createElement('table')
 
     const mapData = require('./carroyage.json')
 
@@ -484,10 +488,17 @@ function displayMap(data, type, scale){
     let zones = []
     let zoneData = []
 
-    console.log(data)
+    // init table data header and border
+    dataTable.style.border = "thin solid #337ab7"
+    const tableHeader = dataTable.createTHead()
+    let rowHeader = tableHeader.insertRow(0)
+    rowHeader.style.border = "thin solid #337ab7"
+    let cellHeader = rowHeader.insertCell(0)
+    cellHeader.innerHTML = "<strong>Zone</strong>"
+
+    console.log(data);
 
     data.forEach(element => {
-
         let exists = false
         let obj = new Object()
 
@@ -505,7 +516,6 @@ function displayMap(data, type, scale){
             }   
         }
     });
-    zoneData.sort()
     
     if (type === 'mono') {
         zones = zoneData
@@ -519,6 +529,7 @@ function displayMap(data, type, scale){
             zoneData.push(obj)                    
         }            
     }
+    zoneData.sort()
     console.log(zoneData)
 
     const ctx = canvas.getContext('2d')
@@ -575,7 +586,7 @@ function displayMap(data, type, scale){
                             } else  if (zoneData[index].quantite >= 16) {
                                 ctx.fillStyle = 'rgba(100, 23, 14, 0.80)'
                             }
-                            ctx.fillRect(posX, posY, 106, 106)
+                            ctx.fillRect(posX, posY, 106, 106) 
                         }                                    
                     }
                 }
@@ -585,6 +596,22 @@ function displayMap(data, type, scale){
             x = 0
         }
     }
+
+    // fill data table
+    if (type == "multi") {
+        // add quantity to data table
+        rowHeader.insertCell(1).innerHTML = "<strong>Quantité</strong>"        
+    }
+    zoneData.forEach(element => {
+        // add the zone to the data table
+        row = dataTable.insertRow()
+        cell = row.insertCell(0)
+        cell.innerHTML = element.zone
+        if (element.quantite > 0) {
+            // add quantity cell to data table
+            row.insertCell(1).innerHTML = element.quantite
+        }
+    })
 
     // set scale
     ctx.scale(scale, scale)
@@ -597,370 +624,7 @@ function displayMap(data, type, scale){
         dlButton.style.visibility = "visible"
     } else {
         dlButton.style.visibility = "hidden"
+        document.getElementById('dataTable').innerHTML = ""
+        document.getElementById('dataTable').appendChild(dataTable)
     }
-}
-
-function showYearMap(annee, scale) {
-    const db = require('electron-db')
-    const path = require('path')
-    const location = path.join(__dirname, '/')
-    const canvas = document.querySelector('#carroyage')
-    const divMap = document.getElementById('map')
-    const width = canvas.width = (4218)
-    const height = canvas.height = (5318)
-
-    // load map background
-    const image = new Image()
-    image.src = './assets/img/plan-v2_2019.jpg'
-
-    let x = y = 0;
-
-    const mapData = require('./carroyage.json')
-    const year = parseInt(document.getElementById(annee).value)
-
-    // get data
-    let zones = []
-    let zoneData = []
-    
-    // get data from database
-    if(db.valid('fouilles', location)) {
-        db.getRows('fouilles', location, {
-            date: year
-        }, (succ, data) => {
-            if(succ) {                
-                data.forEach(element => {
-                    let exists = false
-
-                    if (element.zone !== 'Zone') {
-                        for (let i = 0; i < zones.length; i++) {
-                            if (element.zone === zones[i]) {
-                                exists = true
-                            }
-                        }
-                        if (exists === false) {
-                            zones.push(element.zone)
-                        }
-                    }
-                })
-                zones.sort()
-                for (let index = 0; index < zones.length; index++) {
-                    let obj = new Object()
-
-                    obj.zone = zones[index]
-                    obj.quantite = 0
-
-                    zoneData.push(obj)                    
-                }
-            } else {
-                console.log('An error has occured.')
-                console.log(`Message: ${data}`)
-                return null
-            }
-        })
-    }
-
-    const ctx = canvas.getContext('2d');
-
-    // display map with carroyage
-    image.onload = function() {
-        // display map background
-        ctx.drawImage(image, 0, 0);
-
-        // draw grid
-        for (let posY = -10; posY < image.height; posY += 106) {
-            for (let posX = 15; posX < image.width; posX += 106) {
-                ctx.strokeStyle = 'rgb(0, 0, 0)';
-                ctx.strokeRect(posX, posY, 106, 106);
-                ctx.fillStyle = 'black';
-                ctx.font = '36px arial';
-                
-                // display data on map
-                if (mapData.carroyage[y][x] != ".") {
-                    ctx.fillText(mapData.carroyage[y][x], posX+30, posY+65);
-                    for (let index = 0; index < zoneData.length; index++) {
-                        if (zoneData[index].zone == mapData.carroyage[y][x]) {
-                            if (zoneData[index].quantite == 0){
-                                ctx.fillStyle = 'rgba(0,255,0, 0.50)';
-                            } else if (zoneData[index].quantite < 6) {
-                                ctx.fillStyle = 'rgba(254, 254, 177, 0.80)';                                        
-                            } else if (zoneData[index].quantite >= 6 && zoneData[index].quantite < 11) {
-                                ctx.fillStyle = 'rgba(253, 175, 79, 0.80)';                        
-                            } else if (zoneData[index].quantite >= 11 && zoneData[index].quantite < 16) {
-                                ctx.fillStyle = 'rgba(237, 80, 40, 0.80)';
-                            } else  if (zoneData[index].quantite >= 16) {
-                                ctx.fillStyle = 'rgba(100, 23, 14, 0.80)';
-                            }
-                            ctx.fillRect(posX, posY, 106, 106);
-                        }                                    
-                    }
-                }
-                x++;
-            }
-            y++;
-            x = 0;
-        }
-    }
-
-    // set scale
-    ctx.scale(scale, scale)
-
-    // show map
-    divMap.style.visibility = "visible"
-}
-
-function showCategorieMap(categorie, scale) {
-    const db = require('electron-db')
-    const path = require('path')
-    const location = path.join(__dirname, '/')
-    const canvas = document.querySelector('#carroyage')
-    const divMap = document.getElementById('map')
-    const width = canvas.width = (4218);
-    const height = canvas.height = (5318);
-    
-    // load map background
-    const image = new Image();
-    image.src = './assets/img/plan-v2_2019.jpg'
-
-    let x = y = 0;
-
-    const mapData = require('./carroyage.json')
-
-    let zoneData = []
-    
-    // get data from database            
-    if(db.valid('fouilles', location)) {
-        db.getRows('fouilles', location, {
-            categorie: document.getElementById(categorie).value
-        }, (succ, data) => {
-            if(succ) {
-                
-                data.forEach(element => {
-                    let exists = false
-                    let obj = new Object()
-
-                    if (element.zone !== 'Zone') {
-                        for (let i = 0; i < zoneData.length; i++) {
-                            if (element.zone === zoneData[i].zone) {
-                                exists = true
-                                zoneData[i].quantite += element.quantite
-                            }
-                        }
-                        if (exists === false) {
-                            obj.zone = element.zone
-                            obj.quantite = element.quantite
-                            zoneData.push(obj)
-                        }
-                    }
-                });
-                zoneData.sort()
-
-            } else {
-                console.log('An error has occured.')
-                console.log(`Message: ${data}`)
-                return null
-            }
-        })
-    }
-
-    const ctx = canvas.getContext('2d');
-
-    // display map with carroyage
-    image.onload = function() {
-        // display map background
-        ctx.drawImage(image, 0, 0);
-        
-        // legende des couleurs
-        ctx.fillStyle = 'rgba(254, 254, 177, 0.80)';
-        ctx.fillRect(15 + (20*106), -10 + (3*106), 106, 106);
-        ctx.fillStyle = 'black';
-        ctx.font = '36px arial';
-        ctx.fillText("de 1 à 5", 15 + (21*106) + 15, -10 + (3*106) + 65);
-        ctx.fillStyle = 'rgba(253, 175, 79, 0.80)';
-        ctx.fillRect(15 + (20*106), -10 + (4*106), 106, 106);
-        ctx.fillStyle = 'black';
-        ctx.font = '36px arial';
-        ctx.fillText("de 6 à 10", 15 + (21*106) + 15, -10 + (4*106) + 65);
-        ctx.fillStyle = 'rgba(237, 80, 40, 0.80)';
-        ctx.fillRect(15 + (20*106), -10 + (5*106), 106, 106);
-        ctx.fillStyle = 'black';
-        ctx.font = '36px arial';
-        ctx.fillText("de 11 à 15", 15 + (21*106) + 15, -10 + (5*106) + 65);
-        ctx.fillStyle = 'rgba(100, 23, 14, 0.80)';
-        ctx.fillRect(15 + (20*106), -10 + (6*106), 106, 106);
-        ctx.fillStyle = 'black';
-        ctx.font = '36px arial';
-        ctx.fillText("supérieur à 16", 15 + (21*106) + 15, -10 + (6*106) + 65);
-
-        // draw grid
-        for (let posY = -10; posY < image.height; posY += 106) {
-            for (let posX = 15; posX < image.width; posX += 106) {
-                ctx.strokeStyle = 'rgb(0, 0, 0)';
-                ctx.strokeRect(posX, posY, 106, 106);
-                ctx.fillStyle = 'black';
-                ctx.font = '36px arial';
-                
-                // display data on map
-                if (mapData.carroyage[y][x] != ".") {
-                    ctx.fillText(mapData.carroyage[y][x], posX+30, posY+65);
-                    for (let index = 0; index < zoneData.length; index++) {
-                        if (zoneData[index].zone == mapData.carroyage[y][x]) {
-                            if (zoneData[index].quantite == 0){
-                                ctx.fillStyle = 'rgba(0,255,0, 0.50)';
-                            } else if (zoneData[index].quantite < 6) {
-                                ctx.fillStyle = 'rgba(254, 254, 177, 0.80)';                                        
-                            } else if (zoneData[index].quantite >= 6 && zoneData[index].quantite < 11) {
-                                ctx.fillStyle = 'rgba(253, 175, 79, 0.80)';                        
-                            } else if (zoneData[index].quantite >= 11 && zoneData[index].quantite < 16) {
-                                ctx.fillStyle = 'rgba(237, 80, 40, 0.80)';
-                            } else  if (zoneData[index].quantite >= 16) {
-                                ctx.fillStyle = 'rgba(100, 23, 14, 0.80)';
-                            }
-                            ctx.fillRect(posX, posY, 106, 106);
-                        }                                    
-                    }
-                }
-                x++;
-            }
-            y++;
-            x = 0;
-        }
-    }
-    
-    // set scale
-    ctx.scale(scale, scale)
-
-    // show map
-    divMap.style.visibility = "visible"
-}
-
-function showYearCategorieMap(annee, categorie, scale) {
-    const db = require('electron-db')
-    const path = require('path')
-    const location = path.join(__dirname, '/')
-    const canvas = document.querySelector('#carroyage')
-    const divMap = document.getElementById('map')
-    canvas.width = (4218)
-    canvas.height = (5318)
-
-    // load map
-    const image = new Image()
-    image.src = './assets/img/plan-v2_2019.jpg'
-
-    let x = y = 0
-
-    const year = parseInt(document.getElementById(annee).value)
-    const mapData = require('./carroyage.json')
-
-    let zoneData = []
-            
-    if(db.valid('fouilles', location)) {
-        db.getRows('fouilles', location, {
-            date: year,
-            categorie: document.getElementById(categorie).value
-        }, (succ, data) => {
-            if(succ) {                
-                data.forEach(element => {
-                    let exists = false
-                    let obj = new Object()
-
-                    if (element.zone !== 'Zone') {
-                        for (let i = 0; i < zoneData.length; i++) {
-                            if (element.zone === zoneData[i].zone) {
-                                exists = true
-                                zoneData[i].quantite += element.quantite
-                            }
-                        }
-                        if (exists === false) {
-                            obj.zone = element.zone
-                            obj.quantite = element.quantite
-                            zoneData.push(obj)
-                        }
-                    }
-                });
-                zoneData.sort()
-
-            } else {
-                console.log('An error has occured.')
-                console.log(`Message: ${data}`)
-                return null
-            }
-        })
-    }
-
-    const ctx = canvas.getContext('2d')
-
-    // display map with carroyage
-    image.onload = function() {
-        ctx.drawImage(image, 0, 0)
-        
-        // legende des couleurs
-        ctx.fillStyle = 'rgba(254, 254, 177, 0.80)'
-        ctx.fillRect(15 + (20*106), -10 + (3*106), 106, 106)
-        ctx.fillStyle = 'black'
-        ctx.font = '36px arial'
-        ctx.fillText("de 1 à 5", 15 + (21*106) + 15, -10 + (3*106) + 65)
-        ctx.fillStyle = 'rgba(253, 175, 79, 0.80)'
-        ctx.fillRect(15 + (20*106), -10 + (4*106), 106, 106)
-        ctx.fillStyle = 'black'
-        ctx.font = '36px arial'
-        ctx.fillText("de 6 à 10", 15 + (21*106) + 15, -10 + (4*106) + 65)
-        ctx.fillStyle = 'rgba(237, 80, 40, 0.80)'
-        ctx.fillRect(15 + (20*106), -10 + (5*106), 106, 106)
-        ctx.fillStyle = 'black'
-        ctx.font = '36px arial'
-        ctx.fillText("de 11 à 15", 15 + (21*106) + 15, -10 + (5*106) + 65)
-        ctx.fillStyle = 'rgba(100, 23, 14, 0.80)'
-        ctx.fillRect(15 + (20*106), -10 + (6*106), 106, 106)
-        ctx.fillStyle = 'black'
-        ctx.font = '36px arial'
-        ctx.fillText("supérieur à 16", 15 + (21*106) + 15, -10 + (6*106) + 65)
-
-        // draw grid
-        for (let posY = -10; posY < image.height; posY += 106) {
-            for (let posX = 15; posX < image.width; posX += 106) {
-                ctx.strokeStyle = 'rgb(0, 0, 0)'
-                ctx.strokeRect(posX, posY, 106, 106)
-                ctx.fillStyle = 'black'
-                ctx.font = '36px arial'
-                
-                // display data on map
-                if (mapData.carroyage[y][x] != ".") {
-                    ctx.fillText(mapData.carroyage[y][x], posX+30, posY+65);
-                    for (let index = 0; index < zoneData.length; index++) {
-                        if (zoneData[index].zone == mapData.carroyage[y][x]) {
-                            if (zoneData[index].quantite == 0){
-                                ctx.fillStyle = 'rgba(0,255,0, 0.50)'
-                            } else if (zoneData[index].quantite < 6) {
-                                ctx.fillStyle = 'rgba(254, 254, 177, 0.80)'
-                            } else if (zoneData[index].quantite >= 6 && zoneData[index].quantite < 11) {
-                                ctx.fillStyle = 'rgba(253, 175, 79, 0.80)'                        
-                            } else if (zoneData[index].quantite >= 11 && zoneData[index].quantite < 16) {
-                                ctx.fillStyle = 'rgba(237, 80, 40, 0.80)'
-                            } else  if (zoneData[index].quantite >= 16) {
-                                ctx.fillStyle = 'rgba(100, 23, 14, 0.80)'
-                            }
-                            ctx.fillRect(posX, posY, 106, 106)
-                        }                                    
-                    }
-                }
-                x++
-            }
-            y++
-            x = 0
-        }
-    }
-
-    // set scale
-    ctx.scale(scale, scale)
-
-    // show map
-    divMap.style.visibility = "visible"
-}
-
-function downloadCanvas() {
-    const download = document.getElementById('download')
-    const canvas = document.getElementById('carroyage')
-
-    const img = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream")
-    download.setAttribute("href", img)
 }
