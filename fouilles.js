7eb2f82a55e4e3a9d891dae1b28f89f4070590a0
philippeBron <philippe.bron@gmail.com>
2022-04-24 09:46:34 +0200

    ✨ select data faile to load

--- fouilles.js
+++ fouilles.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const { rejects } = require('assert')
 
 const initApp = () => {
@@ -91,7 +93,73 @@ const initApp = () => {
     document.getElementById('dataTable').style.visibility = "hidden"
 }
 
- const selectorCheck = () => {
+const dataLoad = (file) => {
+    const readXLsxFile = require('read-excel-file/node')
+    const db = require('electron-db')
+
+    const location = path.join(__dirname, './')
+    db.createTable('fouilles', location, (succ, msg) => {
+        console.log("Success: " + succ)
+        console.log("Message: " + msg)
+    })
+
+    if(db.valid('fouilles', location)) {         
+        db.clearTable('fouilles', location, (succ, msg) => {
+            console.log(`Success: ${succ}`)
+            console.log(`Message: ${msg}`)
+        })
+    }
+
+    readXLsxFile(file).then((rows) => {
+        rows.forEach(element => {
+            let obj = new Object()
+
+            obj.zone = element[0]
+            obj.categorie = element[1]
+            obj.sousCategorie = element[2]
+            obj.quantite = element[3]
+            obj.complement = element[4]
+            if ( element[5] !== null) {
+                obj.us = element[5].toString()                
+            } else {
+                obj.us = element[5]
+            }
+            obj.date = excelDateToJSDate(element[6]).getFullYear()
+
+            console.log(obj)
+            
+            if(db.valid('fouilles', location)) {
+                db.insertTableContent('fouilles', location, obj, (succ, msg) => {
+                    console.log(`Success: ${succ}`)
+                    console.log(`Message: ${msg}`)
+                })
+            }
+        })
+        console.log(`Données chargées.`)
+        window.close()
+    })
+}
+
+const excelDateToJSDate = (serial) => {
+    var utc_days  = Math.floor(serial - 25569)
+    var utc_value = utc_days * 86400;                                        
+    var date_info = new Date(utc_value * 1000)
+ 
+    var fractional_day = serial - Math.floor(serial) + 0.0000001
+ 
+    var total_seconds = Math.floor(86400 * fractional_day)
+ 
+    var seconds = total_seconds % 60
+ 
+    total_seconds -= seconds
+ 
+    var hours = Math.floor(total_seconds / (60 * 60))
+    var minutes = Math.floor(total_seconds / 60) % 60
+ 
+    return new Date(date_info.getFullYear(), date_info.getMonth(), date_info.getDate(), hours, minutes, seconds)
+ }
+
+const selectorCheck = () => {
     const db = require('electron-db')
     const path = require('path')
     const location = path.join(__dirname, '/')
--- main.js
+++ main.js
@@ -59,6 +59,28 @@ const createHelpWindow = () => {
     })
 }
 
+
+const createLoadWindow = () => {
+    loadWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        title: 'Chargement',
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
+    loadWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'loadWindow.html'),
+        protocol: 'file:',
+        slashes: true        
+    }))
+
+    // Clear loadWindow on quit
+    loadWindow.on('close', ()   => {
+        loadWindow = null
+    })
+}
+
 // Catch data:file
 ipcMain.on('data:load', (e, file) => {
     loadDB(file)
@@ -81,7 +103,8 @@ const mainMenuTemplate = [
             {
                 label: 'Chargment des données',
                 click() {
-                    dataLoab('./base_de_donnees_compilee.xlsx')
+                    // dataLoad('./base_de_donnees_compilee.xlsx')
+                    createLoadWindow()
                 }
             },
             {
@@ -136,51 +159,6 @@ if (process.env.NODE_ENV != 'production') {
     })
 }
 
-const dataLoab = (file) => {
-    const readXLsxFile = require('read-excel-file/node')
-    const db = require('electron-db')
-    const location = path.join(__dirname, './')
-    db.createTable('fouilles', location, (succ, msg) => {
-        console.log("Success: " + succ)
-        console.log("Message: " + msg)
-    })
-
-    if(db.valid('fouilles', location)) {         
-        db.clearTable('fouilles', location, (succ, msg) => {
-            console.log(`Success: ${succ}`)
-            console.log(`Message: ${msg}`)
-        })
-    }
-
-    readXLsxFile(file).then((rows) => {
-        rows.forEach(element => {
-            let obj = new Object()
-
-            obj.zone = element[0]
-            obj.categorie = element[1]
-            obj.sousCategorie = element[2]
-            obj.quantite = element[3]
-            obj.complement = element[4]
-            if ( element[5] !== null) {
-                obj.us = element[5].toString()                
-            } else {
-                obj.us = element[5]
-            }
-            obj.date = excelDateToJSDate(element[6]).getFullYear()
-
-            // console.log(obj)
-            
-            if(db.valid('fouilles', location)) {
-                db.insertTableContent('fouilles', location, obj, (succ, msg) => {
-                    // console.log(`Success: ${succ}`)
-                    // console.log(`Message: ${msg}`)
-                })
-            }
-        })
-        console.log(`Données chargées.`)
-    })
-}
-
 const carroyageLoad = (file) => {
     const readXLsxFile = require('read-excel-file/node')
     const db = require('electron-db')
@@ -216,22 +194,3 @@ const carroyageLoad = (file) => {
         })
     })
 }
-
-const excelDateToJSDate = (serial) => {
-    var utc_days  = Math.floor(serial - 25569)
-    var utc_value = utc_days * 86400;                                        
-    var date_info = new Date(utc_value * 1000)
- 
-    var fractional_day = serial - Math.floor(serial) + 0.0000001
- 
-    var total_seconds = Math.floor(86400 * fractional_day)
- 
-    var seconds = total_seconds % 60
- 
-    total_seconds -= seconds
- 
-    var hours = Math.floor(total_seconds / (60 * 60))
-    var minutes = Math.floor(total_seconds / 60) % 60
- 
-    return new Date(date_info.getFullYear(), date_info.getMonth(), date_info.getDate(), hours, minutes, seconds)
- }
